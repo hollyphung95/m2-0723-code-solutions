da22334642481f9e338b9821ee75dec2354abe87
Holly Phung <hollyphung95@gmail.com>
2023-07-18 03:58:56 +0000

    forgot to remove the line that clears the timer. removed clearTimeout(timer);

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 const $heading = document.querySelector('.message');
+
 function helloThere() {
   $heading.textContent = 'Hello There';
-  clearTimeout(timer);
 }
 const timer = setTimeout(helloThere(), 2000);
