23037671c5d20d238927f0d46c7650d1a9338ff0
Holly Phung <hollyphung95@gmail.com>
2023-07-26 03:33:42 +0000

    completed javascript-new exercise

---
+++ javascript-new/new-objects.js
@@ -0,0 +1,32 @@
+/* complete the following tasks for each of these built-in JavaScript constructors:
+ Object, Array, Function, String, Number, and Boolean:
+  Call the constructor with new and assign the return value to a variable. */
+const anObject = new Object();
+
+/* Log the value of the instance to the console. */
+console.log('value of anObject:', anObject);
+
+/* Log the typeof the instance to the console. */
+console.log('typeof anObject:', typeof anObject);
+
+/* repeat for Array, Function, String, Number, and Boolean. */
+
+const anArray = new Array();
+console.log('value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
+
+const aFunction = new Function();
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction:', typeof aFunction);
+
+const aString = new String();
+console.log('value of aString:', aString);
+console.log('typeof aString:', typeof aString);
+
+const aNumber = new Number();
+console.log('value of aNumber:', aNumber);
+console.log('typeof aNumber:', typeof aNumber);
+
+const myBool = new Boolean();
+console.log('value of myBool:', myBool);
+console.log('typeof myBool:', typeof myBool);
