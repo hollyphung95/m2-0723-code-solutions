a82c748c3d9a244150f9ac09a95c1e317b9d153f
Holly Phung <hollyphung95@gmail.com>
2023-08-01 19:36:10 +0000

    Although, original submission was a valid way to create a reversed array with .forEach.  Changes allows for me to log values in reverse order without creating a new array, and just the params passed to the .forEach callback function

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -1,14 +1,10 @@
 const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
-const reversedValues = [];
 console.log('In Order:\n');
-values.forEach((val) => {
+values.forEach((val, index, values) => {
   console.log(val);
-  reversedValues.unshift(val);
 });
 
 console.log('\n\nReverse Order:\n');
-reversedValues.forEach((val) => {
-  console.log(val);
+values.forEach((val, index, values) => {
+  console.log(values[index * -1 + values.length - 1]);
 });
-
-console.log(`values:\t\t${values}\nreversed:\t${reversedValues}`);
